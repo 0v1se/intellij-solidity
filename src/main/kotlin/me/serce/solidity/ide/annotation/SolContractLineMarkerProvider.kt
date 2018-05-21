54429c4d3b3a302289659c1245f3d8b4a49405cb
0v1se <dev@0v1se.com>
2018-05-21 14:04:28 +0300

    functions line markers

--- src/main/kotlin/me/serce/solidity/ide/annotation/SolContractLineMarkerProvider.kt
+++ src/main/kotlin/me/serce/solidity/ide/annotation/SolContractLineMarkerProvider.kt
@@ -8,31 +8,56 @@ import com.intellij.psi.PsiElement
 import me.serce.solidity.ide.navigation.findAllImplementations
 import me.serce.solidity.ide.navigation.findImplementations
 import me.serce.solidity.lang.psi.SolContractDefinition
+import me.serce.solidity.lang.psi.SolFunctionDefinition
+import me.serce.solidity.lang.resolve.function.SolFunctionResolver
 
 class SolContractLineMarkerProvider : LineMarkerProvider {
   override fun getLineMarkerInfo(element: PsiElement): LineMarkerInfo<*>? = null
 
   override fun collectSlowLineMarkers(elements: List<PsiElement>, result: MutableCollection<LineMarkerInfo<PsiElement>>) {
     for (el in elements) {
-      if (el !is SolContractDefinition) {
-        continue
+      when (el) {
+        is SolContractDefinition -> {
+          val identifier = el.identifier
+          if (identifier != null) {
+            val targets = el.findImplementations()
+            if (targets.findFirst() != null) {
+              val info = NavigationGutterIconBuilder
+                .create(AllIcons.Gutter.OverridenMethod)
+                .setTargets(el.findAllImplementations())
+                .setPopupTitle("Go to implementation")
+                .setTooltipText("Has implementations")
+                .createLineMarkerInfo(identifier)
+              result.add(info)
+            }
+          }
+        }
+        is SolFunctionDefinition -> {
+          val identifier = el.identifier
+          if (identifier != null) {
+            val overriden = SolFunctionResolver.collectOverriden(el)
+            if (!overriden.isEmpty()) {
+              val info = NavigationGutterIconBuilder
+                .create(AllIcons.Gutter.OverridingMethod)
+                .setTargets(overriden)
+                .setPopupTitle("Go to overriden functions")
+                .setTooltipText("Overrides function")
+                .createLineMarkerInfo(identifier)
+              result.add(info)
+            }
+            val overrides = SolFunctionResolver.collectOverrides(el)
+            if (!overrides.isEmpty()) {
+              val info = NavigationGutterIconBuilder
+                .create(AllIcons.Gutter.OverridenMethod)
+                .setTargets(overrides)
+                .setPopupTitle("Is overriden")
+                .setTooltipText("Is overriden in subcontracts")
+                .createLineMarkerInfo(identifier)
+              result.add(info)
+            }
+          }
+        }
       }
-      val identifier = el.identifier
-      if (identifier == null) {
-        return
-      }
-      val targets = el.findImplementations()
-      if (targets.findFirst() == null) {
-        continue
-      }
-
-      val info = NavigationGutterIconBuilder
-        .create(AllIcons.Gutter.OverridenMethod)
-        .setTargets(el.findAllImplementations())
-        .setPopupTitle("Go to implementation")
-        .setTooltipText("Has implementations")
-        .createLineMarkerInfo(identifier)
-      result.add(info)
     }
   }
 }
